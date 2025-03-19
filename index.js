739bb7f74a52d15148d97d3ef986e8b7761b15ce
AbdullahHamadax <abdallahamada114@gmail.com>
2025-03-20 01:36:54 +0200

    feat: finish entire project! :D

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function updateSlider() {
 
 slider.addEventListener("input", updateSlider);
 
-let characters = [
+const characters = [
   "A",
   "B",
   "C",
@@ -65,19 +65,125 @@ let characters = [
   "x",
   "y",
   "z",
+  "0",
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "~",
+  "",
+  "!",
+  "@",
+  "#",
+  "$",
+  "%",
+  "^",
+  "&",
+  "*",
+  "(",
+  ")",
+  "_",
+  "-",
+  "+",
+  "=",
+  "{",
+  "[",
+  "}",
+  "]",
+  ",",
+  "|",
+  ":",
+  ";",
+  "<",
+  ">",
+  ".",
+  "?",
+  "/",
 ];
 
 let password = document.getElementById("generated-password");
-let emptyString = "";
+let upperCaseCheckbox = document.getElementById("uppercase-checkbox");
+let lowerCaseCheckbox = document.getElementById("lowercase-checkbox");
+let numberCheckbox = document.getElementById("numbers-checkbox");
+let symbolCheckbox = document.getElementById("symbols-checkbox");
 
 function generateRandomPassword() {
-  emptyString = "";
-  let charLength = document.getElementById("charLengthSlider").value;
+  let strengthCounter = 0;
 
-  for (let index = 0; index < charLength; index++) {
-    let randomIndex = Math.floor(Math.random() * characters.length); // range is gonna be from 0 - 52
-    emptyString += characters[randomIndex];
+  let charLength = document.getElementById("charLengthSlider").value;
+  let possibleCharacters = [];
+
+  if (lowerCaseCheckbox.checked) {
+    possibleCharacters = possibleCharacters.concat(characters.slice(26, 52));
+    strengthCounter++;
+  }
+  if (upperCaseCheckbox.checked) {
+    possibleCharacters = possibleCharacters.concat(characters.slice(0, 26));
+    strengthCounter++;
+  }
+  if (numberCheckbox.checked) {
+    possibleCharacters = possibleCharacters.concat(characters.slice(52, 62));
+    strengthCounter++;
+  }
+  if (symbolCheckbox.checked) {
+    possibleCharacters = possibleCharacters.concat(characters.slice(62));
+    strengthCounter++;
+  }
+
+  if (possibleCharacters.length === 0) {
+    password.textContent = "Please select at least one option!!!";
+    return;
+  }
+
+  let emptyString = "";
+
+  for (let i = 0; i < charLength; i++) {
+    let randomIndex = Math.floor(Math.random() * possibleCharacters.length);
+    emptyString += possibleCharacters[randomIndex];
   }
 
   password.textContent = emptyString;
+  displayPasswordStrength(strengthCounter, charLength);
+  console.log(charLength);
+}
+
+function copyToClipboard() {
+  navigator.clipboard.writeText(password.textContent);
+}
+
+let firstBar = document.getElementById("first-bar");
+let secondbar = document.getElementById("second-bar");
+let thirdbar = document.getElementById("third-bar");
+let fourthbar = document.getElementById("fourth-bar");
+let currentPasswordStrength = document.getElementById("password-strength");
+
+function displayPasswordStrength(strengthCounter, charLength) {
+  firstBar.style.backgroundColor = "transparent";
+  secondbar.style.backgroundColor = "transparent";
+  thirdbar.style.backgroundColor = "transparent";
+  fourthbar.style.backgroundColor = "transparent";
+  if (strengthCounter === 1 || charLength <= 6) {
+    firstBar.style.backgroundColor = "green";
+    currentPasswordStrength.textContent = "weak";
+  } else if (strengthCounter === 2 || charLength <= 6) {
+    firstBar.style.backgroundColor = "yellow";
+    secondbar.style.backgroundColor = "yellow";
+    currentPasswordStrength.textContent = "moderate";
+  } else if (strengthCounter === 3 || charLength <= 10) {
+    firstBar.style.backgroundColor = "orange";
+    secondbar.style.backgroundColor = "orange";
+    thirdbar.style.backgroundColor = "orange";
+    currentPasswordStrength.textContent = "medium";
+  } else if (strengthCounter === 4 || charLength > 15) {
+    firstBar.style.backgroundColor = "red";
+    secondbar.style.backgroundColor = "red";
+    thirdbar.style.backgroundColor = "red";
+    fourthbar.style.backgroundColor = "red";
+    currentPasswordStrength.textContent = "strong";
+  }
 }
